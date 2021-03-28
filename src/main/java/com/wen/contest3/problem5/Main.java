930f463a01641f70b06658a064b6c1702b732645
wen <1280252178@qq.com>
2021-03-28 22:23:52 +0800

    修改3-5，使结果总是返回最大值，但依旧没有通过

--- src/main/java/com/wen/contest3/problem5/Main.java
+++ src/main/java/com/wen/contest3/problem5/Main.java
@@ -1,11 +1,12 @@
 package com.wen.contest3.problem5;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
     /**
      * 未通过，原因不明
-     * @param args
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,17 +14,17 @@ public class Main {
         int T = Integer.parseInt(scanner.nextLine());
         for (int t = 0; t < T; t++) {
             String[] numsStr = scanner.nextLine().split("");
-            int[] arr;
+            Integer[] arr;
             boolean minus;
             if (numsStr[0].equals("-")) {
-                arr = new int[numsStr.length - 1];
+                arr = new Integer[numsStr.length - 1];
                 for (int i = 0; i < arr.length; i++) {
                     arr[i] = Integer.parseInt(numsStr[i + 1]);
                 }
                 minus = true;
             }
             else {
-                arr = new int[numsStr.length];
+                arr = new Integer[numsStr.length];
                 for (int i = 0; i < arr.length; i++) {
                     arr[i] = Integer.parseInt(numsStr[i]);
                 }
@@ -41,13 +42,19 @@ public class Main {
         }
     }
 
-    private int getResult(int[] arr) {
+    private int getResult(Integer[] arr) {
         int len = arr.length;
         boolean[] used = new boolean[len];
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
 
         return getResult(arr, used, 0, len);
     }
-    private int getResult(int[] arr, boolean[] used, int num, int offset) {
+    private int getResult(Integer[] arr, boolean[] used, int num, int offset) {
         int len = arr.length;
         for (int i = 0; i < len; i++) {
             if (!used[i]) {
